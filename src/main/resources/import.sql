479b383b6e64c0fc95a58256028bc87c1a6349a0
Fernando Batista <fernandohsbatista@gmail.com>
2025-10-13 07:49:52 -0300

    infra: add restaurants on import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 insert into cuisines (name) values ('Japanese');
 insert into cuisines (name) values ('Chinese');
 insert into cuisines (name) values ('Italian');
+insert into restaurants (name, delivery_tax) values ('Hitaka food', 1.5);
+insert into restaurants (name, delivery_tax) values ('Aikiko', 2.5);
