a6acd1dcab83404b488627738f540dbdada02f40
Fernando Batista <fernandohsbatista@gmail.com>
2025-10-22 08:57:08 -0300

    feat: set response status code on controllers

--- src/main/java/com/fhsbatista/fooddelivery/api/controller/CuisineController.java
+++ src/main/java/com/fhsbatista/fooddelivery/api/controller/CuisineController.java
@@ -3,9 +3,10 @@ package com.fhsbatista.fooddelivery.api.controller;
 import com.fhsbatista.fooddelivery.domain.model.Cuisine;
 import com.fhsbatista.fooddelivery.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,8 +16,15 @@ public class CuisineController {
     @Autowired
     private CuisineRepository repository;
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
     List<Cuisine> list() {
         return repository.list();
+    }
+
+    @GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    ResponseEntity<Cuisine> find(@PathVariable("id") Long id) {
+        final var cuisine = repository.findById(id);
+        return ResponseEntity.ok(cuisine);
     }
 }
