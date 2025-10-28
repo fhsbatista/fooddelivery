f4d33c2c8277aad9d526446d17b3448a6dfd1725
Fernando Batista <fernandohsbatista@gmail.com>
2025-10-28 06:48:46 -0300

    feat: return 409 on delete cuisine when it cannot be deleted

--- src/main/java/com/fhsbatista/fooddelivery/api/controller/CuisineController.java
+++ src/main/java/com/fhsbatista/fooddelivery/api/controller/CuisineController.java
@@ -4,6 +4,7 @@ import com.fhsbatista.fooddelivery.domain.model.Cuisine;
 import com.fhsbatista.fooddelivery.domain.repository.CuisineRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -64,7 +65,12 @@ public class CuisineController {
             return ResponseEntity.notFound().build();
         }
 
-        repository.delete(cuisine);
+        try {
+            repository.delete(cuisine);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
         return ResponseEntity.noContent().build();
     }
 }
